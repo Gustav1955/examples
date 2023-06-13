26a1f9143d56e6d143df6a7a64d1dc7822ff8878
Алексей Апекишев <apekishev1@mail.ru>
2023-06-13 15:25:20 +0300

    Пример №7 выбор max

---
+++ examples007/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = new Random().Next(1, 500);
+Console.WriteLine(a);
+
+int b = new Random().Next(1, 500);
+Console.WriteLine(b);
+
+int d = new Random().Next(1, 500);
+Console.WriteLine(d);
+
+int c = new Random().Next(1, 500);
+Console.WriteLine(c);
+ 
+int max = a;
+
+ if (b > max) max = b;
+ if (c > max) max = c;
+ if (d > max) max = d;
+
+Console.Write("max = ");
+Console.WriteLine(max);
