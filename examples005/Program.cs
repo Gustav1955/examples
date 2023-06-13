df6f8d2edbfc373afcab10991860c3bed065657b
Алексей Апекишев <apekishev1@mail.ru>
2023-06-13 13:43:51 +0300

    Пример №5 rendom

---
+++ examples005/Program.cs
@@ -0,0 +1,7 @@
+﻿double numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.Write("результат деления=");
+Console.WriteLine(result);
