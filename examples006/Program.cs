f47b143fa9aaf6fb22aac52de3d460228178d95e
Алексей Апекишев <apekishev1@mail.ru>
2023-06-13 14:43:53 +0300

    Пример №6 введен оператор if

---
+++ examples006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лошара")
+{
+    Console.WriteLine("пошел на хуй билядь!!!  ");
+}
+else 
+{
+    Console.Write("Да ты шикарный поцц  ");
+    Console.WriteLine(username);
+
+}
